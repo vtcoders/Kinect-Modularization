14357e625ea01b0fe45056ca591467f4636f4062
Junho Lee <jlee123@vt.edu>
2017-06-27 16:55:29 +0900

    initiate reconstruction

--- newModule/kinectModule/kinectModule/Properties/AssemblyInfo.cs
+++ newModule/kinectModule/kinectModule/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("kinectModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("kinectModule")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("da401db1-07fd-4f44-b508-ada7c5c0b128")]
+[assembly: Guid("8409b55a-6859-450f-be94-afd3bdea0381")]
 
 // Version information for an assembly consists of the following four values:
 //
